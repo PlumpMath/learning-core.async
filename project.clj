d92b73df27cf0379d24af4603440d1365a1c529a
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2013-09-14 23:43:13 +0200

    Changed to master version of core.async and clojurescript 1878

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories {"sonatype-oss-public"
+                 "https://oss.sonatype.org/content/groups/public/"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1859"]
-                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
+                 [org.clojure/clojurescript "0.0-1878"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
                  [domina "1.0.1"]
                  ]
   :plugins [[lein-cljsbuild "0.3.3"]]
